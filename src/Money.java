b1e6c0273ad3db8dbb83ea243ab14d56146ef65b
t.mioki <t.mioki@gmail.com>
2012-07-01 18:58:56 +0900

    第17章完

--- src/Money.java
+++ src/Money.java
@@ -12,26 +12,35 @@ class Money implements Expression{
 		Money money = (Money)object;
 		return amount==money.amount && currency.equals(money.currency());
 	}
+	
 	static Money dollar(int amount){
 		return new Money(amount, "USD");
 	}
+	
 	static Money franc(int amount){
 		return new Money(amount, "CHF");
 	}
+	
 	public Money times(int multiplier){
 		return new Money(amount * multiplier, currency);
 	}
+	
 	String currency(){
 		return currency;
 	}
+	
 	public String toString(){  // for Debug
 		return amount + " " + currency;
 	}
-	Expression plus(Money addend){
+	
+	public Expression plus(Expression addend){
 		return new Sum(this, addend);
 	}
+	
 	public Money reduce(Bank bank, String to){
 		int rate = bank.rate(currency, to);
 		return new Money(amount / rate, to);
 	}
+	
+	
 }
--- src/Sum.java
+++ src/Sum.java
@@ -1,12 +1,22 @@
 public class Sum implements Expression{
-	Money augend;
-	Money addend;
-	Sum(Money augend, Money addend){
+	Expression augend;
+	Expression addend;
+	
+	Sum(Expression augend, Expression addend){
 		this.augend = augend;
 		this.addend = addend;
 	}
+	
 	public Money reduce(Bank bank, String to){
-		int amount = augend.amount + addend.amount;
+		int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
 		return new Money(amount, to);
+	}
+	
+	public Expression plus(Expression addend){
+		return new Sum(this, addend);
+	}
+	
+	public Expression times(int multiplier){
+		return new Sum(augend.times(multiplier), addend.times(multiplier));
 	}
 }
